b509658fe044997970c265675891abc6026d4afe
Leonardo Pires Tirado <leonardo.tirado@docente.pr.senac.br>
2026-04-20 21:41:35 -0300

    Fazendo um crud simples

---
+++ sistema_biblioteca_terminal/sistema_biblioteca.py
@@ -0,0 +1,57 @@
+#BIBLIOTECAS
+import csv
+import os
+
+#VARIAVEL GLOBAL
+ARQUIVO = "livros.csv"
+
+# Criar arquivo se não existir
+def inicializar_arquivo():
+    if not os.path.exists(ARQUIVO):
+        with open(ARQUIVO, "w", newline="", encoding="utf-8") as f:
+            writer = csv.writer(f)
+            writer.writerow(["id", "titulo", "autor", "status"])
+
+
+#CREATE
+
+def gerar_id():
+    try:
+        with open(ARQUIVO, "r", encoding="utf-8") as f:
+            reader = list(csv.reader(f))
+            if len(reader) <= 1:
+                return 1
+            return int (reader[-1][0]) + 1
+    except:
+        1
+
+def cadastrar_livro(): 
+    titulo = input("Titulo: ")
+    autor = input("Autor: ")
+    id_livro = gerar_id()
+
+    with open(ARQUIVO, "a", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow([id_livro, titulo, autor, "Disponivel" ])
+
+    print("Livro cadastrado com sucesso!")
+
+
+
+def menu(): 
+    # while True:
+    print("\n=== SISTEMA DE BIBLIOTECA ===")
+    print("1 - Cadastrar livro")
+    print("2 - Listar livros")
+    print("3 - Emprestar/Devolver livro")
+    print("4 - Remover livro")
+    print("0 - Sair")
+
+    # Variavel que recebe opcao de escolha do usuario
+    opcao = input("Escolha: ")
+    print ("Opção escolhida:",opcao)
+
+    if opcao == "1":
+        cadastrar_livro()
+    
+menu()
